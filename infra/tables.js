7fe5cd486c67f27b4e0ea8940871d5ecb5124678
GuilhermeVitorino <gui_vito@hotmail.com>
2021-04-12 15:54:02 -0300

    add date and creation_date fields

--- infra/tables.js
+++ infra/tables.js
@@ -6,7 +6,7 @@ class Tables {
     }
 
     createCustomerService() {
-        const sql = 'CREATE TABLE IF NOT EXISTS customer_service (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS customer_service (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, date datetime NOT NULL, creation_date datetime NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY(id))'
 
         this.connection.query(sql, (error) => {
             if (error) {
--- models/customer-service.js
+++ models/customer-service.js
@@ -1,9 +1,13 @@
+const moment = require('moment');
 const connection = require('../infra/connection')
 
 class CustomerService {
     add(customerService) {
+        const creation_date = moment().format('YYYY-MM-DD HH:MM:SS')
+        const date = moment(customerService.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const customerServiceDated = {...customerService, creation_date, date }
         const sql = 'INSERT INTO customer_service SET ?'
-        connection.query(sql, customerService, (erro, result) => {
+        connection.query(sql, customerServiceDated, (erro, result) => {
             if (erro) {
                 console.log(erro)
             } else {
